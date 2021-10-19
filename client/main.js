608d2dfec082ec39ef477e08dee702e3386335a3
RajathaDH <59072510+RajathaDH@users.noreply.github.com>
2021-10-19 10:57:37 +0530

    Add messaging functionality over web sockets

--- client/main.js
+++ client/main.js
@@ -5,11 +5,16 @@ const messageInputElement = document.querySelector('#messageInput');
 const SERVER_URL = 'http://localhost:3000';
 const socket = io(SERVER_URL);
 
+socket.on('message', data => {
+    addMessage('Other', data.message);
+});
+
 messageFormElement.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const message = messageInputElement.value;
     addMessage('You', message);
+    sendMessage(message);
 
     messageInputElement.value = '';
 });
@@ -18,4 +23,8 @@ function addMessage(sender, message) {
     const messageElement = document.createElement('p');
     messageElement.innerText = `${sender}: ${message}`;
     messagesElement.appendChild(messageElement);
+}
+
+function sendMessage(message) {
+    socket.emit('message', { sender: 'You', message });
 }
