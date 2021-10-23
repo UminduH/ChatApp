c0da8c7daef76f78fe02783ac85b374510507a22
RajathaDH <59072510+RajathaDH@users.noreply.github.com>
2021-10-23 10:01:29 +0530

    Add room functionality

--- server/server.js
+++ server/server.js
@@ -11,6 +11,10 @@ const io = socketio(PORT, {
 
 io.on('connection', (socket) => {
     socket.on('message', data => {
-        socket.broadcast.emit('message', { sender: data.sender, message: data.message });
+        socket.broadcast.to(data.room).emit('message', { sender: data.sender, message: data.message });
+    });
+
+    socket.on('join-room', data => {
+        socket.join(data.room);
     });
 });
